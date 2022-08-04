39a6615fa9c1b77bea5011612e3a446090fb4d73
DmityFilchev <filchev_dmitry@mail.ru>
2022-08-04 16:52:42 +0300

    '04'

--- Task21_1/Task21_1.cpp
+++ Task21_1/Task21_1.cpp
@@ -10,7 +10,7 @@ struct Pay
 	std::string data;
 	int sum;
 };
-void print(std::vector<Pay>&payments)
+void print(std::vector<Pay>& payments)
 {
 	for (int j = 0; j < payments.size(); j++)
 	{
@@ -22,15 +22,72 @@ int main()
 	std::vector<Pay> payments;
 	std::ifstream load("statement.txt");
 	int i = 0;
-	while (!load.eof())
-	{
-		payments.push_back(Pay());
-		std::string text;
-		std::getline(load, text);
-		std::stringstream temp(text);
-		temp >> payments[i].f_name >>payments[i].l_name>> payments[i].data >> payments[i].sum;
-		i++;
+	if (load.is_open()) {
+		while (!load.eof())
+		{
+			payments.push_back(Pay());
+			std::string text;
+			std::getline(load, text);
+			std::stringstream temp(text);
+			temp >> payments[i].f_name >> payments[i].l_name >> payments[i].data >> payments[i].sum;
+			i++;
+		}
 	}
-	print(payments);
+	else 
+	{
+		std::cout << "The file did not open"<<std::endl;
+	}
 	load.close();
+	std::string option;
+	std::cout << "Enter list to display the statement or add to enter data"<<std::endl;
+	std::cin >> option;
+	if (option == "list")
+	{
+		print(payments);
+	}
+	else if (option == "add")
+	{
+		std::string date;
+		std::string f_name;
+		std::string l_name;
+		int sum = 0;
+		std::cout << "Enter a name : ";
+		std::cin>> f_name;
+		std::cout << "Enter a last name : ";
+		std::cin>>l_name;
+		bool ok;
+		do
+		{
+			std::cout << "Date : ";
+			std::cin >> date;
+			int a = stoi(date.substr(0, 2));
+			int b = stoi(date.substr(3, 2));
+			int c = stoi(date.substr(6, 4));
+			if (a > 31 || b > 12 || c > 2025)
+			{
+				std::cout << "Error!!!!\n";
+				ok = true;
+			}
+			else {
+				ok = false;
+			}
+		} while (ok);
+		std::cout << "Sum :";
+		std::cin >> sum;
+		payments.push_back(Pay());
+		payments[i].f_name = f_name;
+		payments[i].l_name = f_name;
+		payments[i].data = date;
+		payments[i].sum = sum;
+		std::ofstream text("statement.txt", std::ios::app);
+		text << f_name << " "<<l_name<<" "<< date << " " << sum << std::endl;
+		text.close();
+		print(payments);
+	}
+	else
+	{
+		std::cout << "Invalid input" << std::endl;
+	}
+
+	
 }
